0cbab0065ec3e8af266889bcb442d0fbb4b69bb6
Bola Ahmed Buari <36057474+bolah2009@users.noreply.github.com>
2022-12-17 13:46:32 +0100

    Add circle ci and ansible playbook to config aws infrastructure.

---
+++ scripts/create-ec2.sh
@@ -0,0 +1,1 @@
+aws ec2 run-instances --image-id ami-0b5eea76982371e91 --count 1 --instance-type t2.nano --key-name udacity_demo_test --security-group-ids sg-046456e8148cb674d --subnet-id subnet-060c7a8b668c01b95
